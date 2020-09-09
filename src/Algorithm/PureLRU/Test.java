ae361aebd642cb998c11dd35cd94dcaaae3d08f6
Chen Yuxi <chenyuxi@do1.com.cn>
2020-09-09 09:41:20 +0800

    更新PureLRU的测试类中测试添加key值为null时的情况

--- src/Algorithm/PureLRU/Test.java
+++ src/Algorithm/PureLRU/Test.java
@@ -2,25 +2,31 @@ package Algorithm.PureLRU;
 
 public class Test  {
     public static void main(String[] args) {
-        PureLRU<Integer,String> PureLRU = new PureLRU<>(3);
-        System.out.println(PureLRU.isEmpty()); // true
-        PureLRU.put(1,"A");
-        PureLRU.put(2,"B");
-        PureLRU.put(3,"C");
-        System.out.println(PureLRU.toString()); // 3,2,1
-        System.out.println(PureLRU.get(1)); // A
-        System.out.println(PureLRU.toString()); // 1,3,2
-        PureLRU.get(2);
-        System.out.println(PureLRU.toString()); // 2,1,3
+        PureLRU<Integer,String> pureLRU = new PureLRU<>(3);
+        System.out.println(pureLRU.isEmpty()); // true
+        pureLRU.put(1,"A");
+        pureLRU.put(2,"B");
+        pureLRU.put(3,"C");
+        System.out.println(pureLRU.toString()); // 3,2,1
+        System.out.println(pureLRU.get(1)); // A
+        System.out.println(pureLRU.toString()); // 1,3,2
+        pureLRU.get(2);
+        System.out.println(pureLRU.toString()); // 2,1,3
 
-        PureLRU.put(4,"D");
-        System.out.println(PureLRU.toString()); // 4,2,1
+        pureLRU.put(4,"D");
+        System.out.println(pureLRU.toString()); // 4,2,1
 
-        PureLRU.put(1,"Z");
-        System.out.println(PureLRU.toString()); // 1,4,2
-        System.out.println(PureLRU.get(1)); // Z
+        pureLRU.put(1,"Z");
+        System.out.println(pureLRU.toString()); // 1,4,2
+        System.out.println(pureLRU.get(1)); // Z
 
-        System.out.println(PureLRU.contains(4)); // true
-        System.out.println(PureLRU.isEmpty()); // false
+        System.out.println(pureLRU.contains(4)); // true
+        System.out.println(pureLRU.isEmpty()); // false
+
+        pureLRU.put(null,"nullVal");
+        System.out.println(pureLRU.toString()); // null,1,4
+        System.out.println(pureLRU.get(null)); // nullVal
+
+
     }
 }
