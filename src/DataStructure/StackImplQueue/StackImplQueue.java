aac47d9dcecb62c1f137b76eabb95990948deb1f
yuc <732298225@qq.com>
2020-09-11 21:08:40 +0800

    剑指 Offer 09: 双栈实现队列

--- src/DataStructure/StackImplQueue/StackImplQueue.java
+++ src/DataStructure/StackImplQueue/StackImplQueue.java
@@ -6,14 +6,12 @@ import java.util.Stack;
  * 双栈实现队列
  *
  * @author yuc
- * <p>
+ *
  * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
- * <p>
- * 来源：力扣（LeetCode）
+ *
  * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- * <p>
+ *
  * 执行用时： 59 ms , 在所有 Java 提交中击败了 48.82% 的用户
  * 内存消耗： 47.6 MB , 在所有 Java 提交中击败了 88.28% 的用户
  */
