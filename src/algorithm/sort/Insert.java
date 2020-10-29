fd2919d3832f673e02ec52c04a8d7b4ca881eaa7
yuc <chenyuxi@do1.com.cn>
2020-10-29 15:50:44 +0800

    update: 插入排序

---
+++ src/algorithm/sort/Insert.java
@@ -0,0 +1,37 @@
+package algorithm.sort;
+
+import algorithm.sort.util.Swaper;
+import algorithm.sort.util.Tester;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序
+ * 时间复杂度：O(n^2)
+ * @Author yuc
+ */
+public class Insert {
+
+    public static void sort(int[] arr){
+        for(int i=0;i<arr.length-1;++i){
+            for(int j=i+1;j>0;--j){
+                if(arr[j-1]>arr[j]){
+                    Swaper.nor(arr,j-1,j);
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] tmp = Tester.randomArr(100000);
+        int[] arr = tmp.clone();
+//        System.out.println(Arrays.toString(arr));
+        long start = System.currentTimeMillis();
+        sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("插入排序结束，耗时" + (end - start) + "ms");
+//        System.out.println(Arrays.toString(arr));
+    }
+}
