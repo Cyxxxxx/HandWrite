eb20893b91a74c0c8d3afd8c8c830fa3452f1726
yuc <chenyuxi@do1.com.cn>
2020-11-05 18:31:31 +0800

    update: 单例模式 - 测试类

---
+++ src/design_patterns/singleton/Tester.java
@@ -0,0 +1,29 @@
+package design_patterns.singleton;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 单例模式 - 测试类
+ */
+public class Tester {
+
+    private static int testCount = 20;
+
+    private static CountDownLatch cdl = new CountDownLatch(testCount);
+
+    public static void LazyManWithoutDCLTester() throws InterruptedException {
+        System.out.println("懒汉式不带DCL测试开始");
+        for(int i=0;i<20;++i){
+            new Thread(()->{
+                System.out.println(LazyManWithoutDCL.getInstance());
+                cdl.countDown();
+            }).start();
+        }
+        cdl.await();
+        System.out.println("懒汉式不带DCL测试结束");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        LazyManWithoutDCLTester();
+    }
+}
